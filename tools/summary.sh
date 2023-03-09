e50587b69de45de25fd417422d29b31b0ab7fcd6
JimmyShi22 <417711026@qq.com>
2023-03-09 16:26:34 +0800

    disable sharding in wasm

--- tools/summary.sh
+++ tools/summary.sh
@@ -9,14 +9,14 @@ echo -e "waitT\t\t" ${waitT}
 echo -e "commitT\t\t" ${commitT}
 echo -e "beforeSeal\t" ${beforeSeal}
 echo "------tx pool (tps)------- "
-cat $1 |grep -iaE "tx_pool_in" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request\/s\\)=' '{print $2}' |awk '{sum+=$1} END {print "in      \t", sum/NR}'
-cat $1 |grep -iaE "tx_pool_seal" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request\/s\\)=' '{print $2}' |awk '{sum+=$1} END {print "seal    \t", sum/NR}'
-cat $1 |grep -iaE "tx_pool_rm" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request\/s\\)=' '{print $2}' |awk '{sum+=$1} END {print "rm      \t", sum/NR}'
+cat $1 |grep -iaE "tx_pool_in" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request/s)=' '{print $2}' |awk '{sum+=$1} END {print "in      \t", sum/NR}'
+cat $1 |grep -iaE "tx_pool_seal" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request/s)=' '{print $2}' |awk '{sum+=$1} END {print "seal    \t", sum/NR}'
+cat $1 |grep -iaE "tx_pool_rm" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request/s)=' '{print $2}' |awk '{sum+=$1} END {print "rm      \t", sum/NR}'
 echo "------storage (tps)------- "
-cat $1 |grep -iaE "create_table_rate_collector" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request\/s\\)=' '{print $2}' |awk '{sum+=$1} END {print "create   \t", sum/NR}'
-cat $1 |grep -iaE "open_table_rate_collector" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request\/s\\)=' '{print $2}' |awk '{sum+=$1} END {print "open    \t", sum/NR}'
-cat $1 |grep -iaE "get_row_table_rate_collector" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request\/s\\)=' '{print $2}' |awk '{sum+=$1} END {print "get      \t", sum/NR}'
-cat $1 |grep -iaE "set_row_table_rate_collector" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request\/s\\)=' '{print $2}' |awk '{sum+=$1} END {print "set      \t", sum/NR}'
+cat $1 |grep -iaE "create_table_rate_collector" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request/s)=' '{print $2}' |awk '{sum+=$1} END {print "create   \t", sum/NR}'
+cat $1 |grep -iaE "open_table_rate_collector" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request/s)=' '{print $2}' |awk '{sum+=$1} END {print "open    \t", sum/NR}'
+cat $1 |grep -iaE "get_row_table_rate_collector" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request/s)=' '{print $2}' |awk '{sum+=$1} END {print "get      \t", sum/NR}'
+cat $1 |grep -iaE "set_row_table_rate_collector" |grep -v "lastQPS(request/s)=0" |awk -F 'lastQPS\\(request/s)=' '{print $2}' |awk '{sum+=$1} END {print "set      \t", sum/NR}'
 echo "-------------------------- "
 blkTxs=$(cat $1 |grep "Report,sea" |awk -F 'txs=' '{print $2}' |awk -F ',' '{print $1}'|awk '{sum+=$1} END {print sum/NR}')
 echo -e "blkTxs\t\t" ${blkTxs}
